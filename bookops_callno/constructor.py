deebd02011ef5fbd226469a7f270cb634bc422b0
klinga <klingaroo@gmail.com>
2021-07-28 10:44:40 -0400

    BPLCallNo & NYPLCallNo init

--- bookops_callno/constructor.py
+++ bookops_callno/constructor.py
@@ -3,30 +3,26 @@
 """
 This module provides the main interface for the package
 """
-from collections import namedtuple
 from typing import Optional, Tuple
 
 from pymarc import Record, Field
 
 from bookops_callno.parser import (
-    get_field,
-    get_main_entry_tag,
     get_audience,
+    get_field,
+    get_form_of_item_code,
     get_language_code,
+    get_main_entry_tag,
+    get_physical_description,
+    get_record_type_code,
 )
 from bookops_callno.errors import CallNoConstructorError
 
 
-MainEntry = namedtuple("MainEntry", ["field", "tag"])
-
-
 class CallNo:
-    def __init__(
-        self, bib: Record = None, library: str = None, requested_call_type: str = "auto"
-    ):
+    def __init__(self, bib: Record = None, requested_call_type: str = "auto"):
         """
-        Call number constructor. The 'library' argument specifies rules the call number
-        should be using. The 'requested_call_type' may specify what type of
+        Call number constructor. The 'requested_call_type' may specify what type of
         the call number should be constructed (fiction, biography, etc.).
         The list below represents only same, shared between BPL & NYPL call number
         patterns and library specific call types that are possible
@@ -34,8 +30,7 @@ class CallNo:
 
         Args:
             bib:                    pymarc.Record object
-            library:                destination library
-            requested_call_type:              type of call number to create; default 'auto';
+            requested_call_type:    type of call number to create; default 'auto';
                                     shared (BPL & NYPL) type patterns:
                                         - auto:             best match
                                         - fic:              fiction
@@ -46,62 +41,58 @@ class CallNo:
                                         - movie:            DVD movie
                                         - tv:               DVD tv
         """
-        if not isinstance(library, str):
-            raise CallNoConstructorError(
-                "Invalid type of 'library' argument used. Must be a string."
-            )
-
-        if library.lower() not in ("bpl", "nyp", "nypl"):
-            raise CallNoConstructorError(
-                "Invalid 'library' value. Use 'bpl' or 'nypl'."
-            )
-
         if not isinstance(requested_call_type, str):
             raise CallNoConstructorError(
                 "Invalid type of 'requested_call_type' argument used. Must be a string."
             )
 
-        self.library = library
-        self.call_type_requested = requested_call_type
-        self.call_type_produced = None
-        self.segment_audn = None
-        self.segment_content = None
-        self.segment_cutter = None
-        self.segment_format = None
-        self.segment_language = None
-        self.segment_subject = None
+        self.audience_info = None
+        self.content_info = None
+        self.cutter_info = None
+        self.form_of_item_info = None
+        self.language_info = None
+        self.physical_desc_info = None
+        self.record_type_info = None
+        self.subject_info = None
 
-        self._prepare(bib)
+        self.callno_field = None
+        self.requested_call_type = requested_call_type
 
-    def _prepare(self, bib: Record) -> None:
+        self._prep(bib)
+
+    def _prep(self, bib: Record) -> None:
         """
         Prepares elements for a call number creation
         """
-        self.segment_audn = self._get_segment_audn(bib)
-        self.segment_cutter = self._get_main_entry(bib)
-        self.segment_language = self._get_segment_language(bib)
+        self.audience_info = self._get_audience_info(bib)
+        self.cutter_info = self._get_main_entry_info(bib)
+        self.form_of_item_info = self._get_form_of_item_info(bib)
+        self.language_info = self._get_language_info(bib)
+        self.physical_desc_info = self._get_physical_desciption(bib)
+        self.record_type_info = self._get_record_type_info(bib)
 
-    def _get_segment_audn(self, bib: Record) -> Optional[str]:
+    def _get_audience_info(self, bib: Record) -> Optional[str]:
         """
         Determines audience call number segment
         """
         audn = get_audience(bib)
         return audn
 
-    def _get_segment_language(self, bib: Record) -> Optional[str]:
+    def _get_form_of_item_info(self, bib: Record) -> Optional[str]:
+        """
+        Determines form of item MARC code
+        """
+        form_of_item = get_form_of_item_code(bib)
+        return form_of_item
+
+    def _get_language_info(self, bib: Record) -> Optional[str]:
         """
         Determines language code of the material
         """
         lang = get_language_code(bib)
         return lang
 
-    def _get_segment_format(self, bib: Record) -> Optional[str]:
-        """
-        Determines format of the material
-        """
-        pass
-
-    def _get_main_entry(self, bib: Record) -> Tuple[str, Field]:
+    def _get_main_entry_info(self, bib: Record) -> Tuple[str, Field]:
         """
         Determines 'main entry' of the bib.
 
@@ -113,23 +104,25 @@ class CallNo:
 
         return field
 
-    def create(self):
+    def _get_physical_description_info(self, bib: Record) -> Optional[str]:
         """
-        Creates call number based on parsed previously data
+        Returns MARC 300 tag value
         """
-        pass
+        physical_desc = get_physical_description(bib)
+        return physical_desc
+
+    def _get_record_type_info(self, bib: Record) -> Optional[str]:
+        """
+        Returns MARC leader record type code
+        """
+        rec_type = get_record_type(bib)
+        return rec_type
 
     def as_pymarc_field(self) -> Field:
         """
         Returns constructed call number as `pymarc.Field` object
         """
         return self.callno_field
-
-    # def as_string(self) -> str:
-    #     """
-    #     Returns constructed call number as string
-    #     """
-    #     return self.callno_field.value()
 
     def __repr__(self) -> str:
         """
@@ -139,3 +132,13 @@ class CallNo:
             return self.callno_field.value()
         else:
             return ""
+
+
+class BPLCallNo(CallNo):
+    def __init__(self, bib: Record, requested_call_type: str):
+        super().__init__(bib, requested_call_type)
+
+
+class NYPLCallNo(CallNo):
+    def __init__(self, bib: Record, requested_call_type: str):
+        super().__init__(bib, requested_call_type)
