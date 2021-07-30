65670f9d02a775141fb425ffbe4b1ba37b4bcc37
klinga <klingaroo@gmail.com>
2021-07-30 11:49:53 -0400

    trailing punctuation & corporate cutters

--- bookops_callno/normalizer.py
+++ bookops_callno/normalizer.py
@@ -9,6 +9,26 @@ from unidecode import unidecode, UnidecodeError
 from bookops_callno.errors import CallNoConstructorError
 
 
+def remove_trailing_punctuation(value: str) -> str:
+    """
+    Removes any trailing periods, commas, etc.
+
+    Args:
+        value:                  string to be processed
+
+    Returns:
+        value
+    """
+    if not isinstance(value, str):
+        raise CallNoConstructorError(
+            "Invalid 'value' type used in argument. Must be a string."
+        )
+
+    while value[-1] in ".,:;-() ":
+        value = value[:-1]
+    return value
+
+
 def normalize_value(value: str) -> str:
     """
     Removes diacritics from string and changes to uppercase
@@ -22,7 +42,8 @@ def normalize_value(value: str) -> str:
 
     try:
         value = unidecode(value, errors="strict")
-        return value.upper()
+        value = remove_trailing_punctuation(value).upper()
+        return value
     except UnidecodeError as exc:
         raise CallNoConstructorError(
             f"Unsupported character encountered. Error: '{exc}'."
@@ -40,7 +61,19 @@ def corporate_name_first_word(field: Field = None) -> Optional[str]:
     Returns:
         name
     """
-    pass
+    if field is None:
+        return None
+    elif not isinstance(field, Field):
+        raise CallNoConstructorError(
+            "Invalid 'field' argument type. Must be pymarc.Field instance."
+        )
+
+    if field.tag != "110":
+        return None
+
+    words = field["a"].strip().split(" ")
+    name = normalize_value(words[0])
+    return name
 
 
 def corporate_name_full(field: Field = None) -> Optional[str]:
@@ -55,7 +88,19 @@ def corporate_name_full(field: Field = None) -> Optional[str]:
     Returns:
         name
     """
-    pass
+    if field is None:
+        return None
+    elif not isinstance(field, Field):
+        raise CallNoConstructorError(
+            "Invalid 'field' argument type. Must be pymarc.Field instance."
+        )
+
+    if field.tag != "110":
+        return None
+
+    phrases = field["a"].strip().split("(")
+    name = normalize_value(phrases[0])
+    return name
 
 
 def corporate_name_initial(field: Field = None) -> Optional[str]:
@@ -69,7 +114,19 @@ def corporate_name_initial(field: Field = None) -> Optional[str]:
     Returns:
         initial
     """
-    pass
+    if field is None:
+        return None
+    elif not isinstance(field, Field):
+        raise CallNoConstructorError(
+            "Invalid 'field' argument type. Must be pymarc.Field instance."
+        )
+
+    if field.tag != "110":
+        return None
+
+    first_chr = field["a"][0]
+    initial = normalize_value(first_chr)
+    return initial
 
 
 def personal_name_initial(field: Field = None) -> Optional[str]:
--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from pymarc import Field
 import pytest
 
 from bookops_callno.errors import CallNoConstructorError
-from bookops_callno.normalizer import normalize_value
+from bookops_callno.normalizer import (
+    corporate_name_first_word,
+    corporate_name_full,
+    corporate_name_initial,
+    normalize_value,
+    remove_trailing_punctuation,
+)
 
 
 @pytest.mark.parametrize("arg", [None, ""])
@@ -32,9 +39,115 @@ def test_normalize_value_unidecode_exception():
         ("ĄąĆćĘęŁłŃńÓóŚśŹźŻż", "AACCEELLNNOOSSZZZZ"),
         ("ÁáÉéÍíÑñÓóÚúý", "AAEEIINNOOUUY"),
         ("ÄäËëḦḧÏïÖöẗÜüṲṳẄẅẌẍŸÿ", "AAEEHHIIOOTUUUUWWXXYY"),
-        ("Metlit︠s︡kai︠a︡, Marii︠a︡.", "METLITSKAIA, MARIIA."),
+        ("Metlit︠s︡kai︠a︡, Marii︠a︡.", "METLITSKAIA, MARIIA"),
         ("szegfű és fahéj", "SZEGFU ES FAHEJ"),
     ],
 )
 def test_normalize_value(arg, expectation):
     assert normalize_value(arg) == expectation
+
+
+def test_corporate_name_first_word_none_field():
+    assert corporate_name_first_word(field=None) is None
+
+
+def test_corporate_name_first_word_invalid_field_type():
+    msg = "Invalid 'field' argument type. Must be pymarc.Field instance."
+    with pytest.raises(CallNoConstructorError) as exc:
+        corporate_name_first_word(field=1)
+    assert msg in str(exc)
+
+
+def test_corporate_name_first_word_invalid_tag():
+    field = Field(tag="710", indicators=["2", "0"], subfields=["a", "Foo."])
+    assert corporate_name_first_word(field=field) is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (["a", "United States.", "b", "Army."], "UNITED"),
+        (["a", "Poland.", "b", "Army."], "POLAND"),
+        (["a", "Green Day (Musical group)"], "GREEN"),
+    ],
+)
+def test_corporate_name_first_word(arg, expectation):
+    field = Field(tag="110", indicators=["2", "0"], subfields=arg)
+    assert corporate_name_first_word(field=field) == expectation
+
+
+def test_corporate_name_full_none_field():
+    assert corporate_name_full(field=None) is None
+
+
+def test_corporate_name_full_invalid_field_type():
+    msg = "Invalid 'field' argument type. Must be pymarc.Field instance."
+    with pytest.raises(CallNoConstructorError) as exc:
+        corporate_name_full(field=1)
+    assert msg in str(exc)
+
+
+def test_corporate_name_full_invalid_tag():
+    field = Field(tag="710", indicators=["2", "0"], subfields=["a", "Foo."])
+    assert corporate_name_full(field=field) is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (["a", "United States.", "b", "Army."], "UNITED STATES"),
+        (["a", "Green Day (Musical group)"], "GREEN DAY"),
+    ],
+)
+def test_corporate_name_full(arg, expectation):
+    field = Field(tag="110", indicators=["2", "0"], subfields=arg)
+    assert corporate_name_full(field=field) == expectation
+
+
+def test_corporate_name_initial_none_field():
+    assert corporate_name_initial(field=None) is None
+
+
+def test_corporate_name_initial_invalid_field_type():
+    msg = "Invalid 'field' argument type. Must be pymarc.Field instance."
+    with pytest.raises(CallNoConstructorError) as exc:
+        corporate_name_initial(field=1)
+    assert msg in str(exc)
+
+
+def test_corporate_name_initial_invalid_tag():
+    field = Field(tag="710", indicators=["2", "0"], subfields=["a", "Foo."])
+    assert corporate_name_initial(field=field) is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [(["a", "United States.", "b", "Army."], "U"), (["a", "Äu"], "A")],
+)
+def test_corporate_name_initial(arg, expectation):
+    field = Field(tag="110", indicators=["2", "0"], subfields=arg)
+    assert corporate_name_initial(field=field) == expectation
+
+
+def test_remove_trailing_puncutation_invalid_type_exception():
+    msg = "Invalid 'value' type used in argument. Must be a string."
+    with pytest.raises(CallNoConstructorError) as exc:
+        remove_trailing_punctuation(value=None)
+    assert msg in str(exc)
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("foo", "foo"),
+        ("foo.", "foo"),
+        ("foo... ", "foo"),
+        ("foo-", "foo"),
+        ("foo, ", "foo"),
+        ("foo; ", "foo"),
+        ("foo (", "foo"),
+        ("foo: ", "foo"),
+    ],
+)
+def test_remove_trailing_puncutation(arg, expectation):
+    assert remove_trailing_punctuation(arg) == expectation
