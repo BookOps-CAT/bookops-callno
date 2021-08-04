2bd015067596e3bfbe7ac29fabf5b7ff33eab96d
klinga <klingaroo@gmail.com>
2021-08-04 13:43:44 -0400

    cutter initial

--- bookops_callno/rules_shared.py
+++ bookops_callno/rules_shared.py
@@ -9,6 +9,7 @@ from bookops_callno.normalizer import (
     corporate_name_first_word,
     corporate_name_initial,
     personal_name_surname,
+    personal_name_initial,
     title_initial,
 )
 
@@ -34,6 +35,31 @@ def callno_cutter_fic(field: Field = None) -> Optional[str]:
         cutter = title_initial(field)
     else:
         cutter = None
+    return cutter
+
+
+def callno_cutter_initial(field: Field = None) -> Optional[str]:
+    """
+    Constructs cutter constiting of the first letter of the main entry.
+
+    Args:
+        field:                          pymarc.Field instance
+
+    Returns:
+        cutter
+    """
+    if field is None or not isinstance(field, Field):
+        return None
+
+    if field.tag == "100":
+        cutter = personal_name_initial(field)
+    elif field.tag == "110":
+        cutter = corporate_name_initial(field)
+    elif field.tag == "245":
+        cutter = title_initial(field)
+    else:
+        cutter = None
+
     return cutter
 
 
--- tests/test_rules_shared.py
+++ tests/test_rules_shared.py
@@ -3,7 +3,11 @@
 import pytest
 from pymarc import Field
 
-from bookops_callno.rules_shared import callno_cutter_fic, callno_cutter_pic
+from bookops_callno.rules_shared import (
+    callno_cutter_fic,
+    callno_cutter_initial,
+    callno_cutter_pic,
+)
 
 
 @pytest.mark.parametrize(
@@ -19,6 +23,30 @@ def test_callno_cutter_fic(tag, inds, subs, expectation):
     assert callno_cutter_fic(field) == expectation
 
 
+@pytest.mark.parametrize("arg", [None, 100])
+def test_callno_cutter_fic_invalid_arg(arg):
+    assert callno_cutter_fic(arg) is None
+
+
+@pytest.mark.parametrize(
+    "tag,inds,subs,expectation",
+    [
+        ("100", ["1", " "], ["a", "Adams, John."], "A"),
+        ("110", ["2", " "], ["a", "Foo Company."], "F"),
+        ("245", ["0", "4"], ["a", "The foo."], "F"),
+        ("246", ["3", " "], ["a", "The spam."], None),
+    ],
+)
+def test_callno_cutter_initial(tag, inds, subs, expectation):
+    field = Field(tag=tag, indicators=inds, subfields=subs)
+    assert callno_cutter_initial(field) == expectation
+
+
+@pytest.mark.parametrize("arg", [None, 123])
+def test_callno_cutter_initial_invalid_arg(arg):
+    assert callno_cutter_initial(arg) is None
+
+
 @pytest.mark.parametrize(
     "tag,inds,subs,expectation",
     [
@@ -30,3 +58,8 @@ def test_callno_cutter_fic(tag, inds, subs, expectation):
 def test_callno_cutter_pic(tag, inds, subs, expectation):
     field = Field(tag=tag, indicators=inds, subfields=subs)
     assert callno_cutter_pic(field) == expectation
+
+
+@pytest.mark.parametrize("arg", [None, 100])
+def test_callno_cutter_pic_invalid_arg(arg):
+    assert callno_cutter_pic(arg) is None
